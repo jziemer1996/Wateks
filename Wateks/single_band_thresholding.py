2b45c6e08509667afc15dd44ef448815c0c4ca64
jziemer1996 <jz199623@gmail.com>
2020-05-29 19:19:49 +0200

    Classified number of flooded pixels

--- Wateks/single_band_thresholding.py
+++ Wateks/single_band_thresholding.py
@@ -12,7 +12,7 @@ Thresholding is used to create a binary image from a grayscale image [1]_.
     :ref:`sphx_glr_auto_examples_applications_plot_thresholding.py`
 
 """
-
+import collections
 
 import matplotlib.pyplot as plt
 import skimage
@@ -24,7 +24,7 @@ import os
 import cv2
 import numpy as np
 from matplotlib import pyplot as plt
-
+import _collections
 
 ######################################################################
 # We illustrate how to apply one of these thresholding algorithms.
@@ -38,8 +38,8 @@ from matplotlib import pyplot as plt
 ### Bei einfachem Ausführen des Moduls muss nichts auskommentiert werden
 ### Bei komplexem Ausführen über water_functions.py Histogramme und print-Funktion auskommentieren
 
-input_folder = "D:/HiWi/01_SALDI/Output_Mpumalanga/"
-input_tif = "Driekoppies_VH_median_filter3_threshold_otsu5"
+input_folder = "F:/HiWi/01_SALDI/Output_Mpumalanga/"
+input_tif = "Driekoppies_VH_median_filter3_threshold_otsu5_binary_otsu17"
 input = input_folder+input_tif
 image = io.imread(input)
 thresh = threshold_triangle(image)
@@ -58,5 +58,26 @@ ax[1].set_title('Histogram')
 ax[1].axvline(thresh, color='r')
 
 plt.show()
+
+
+# Number of pixels on x_axis
+x_axis_len = image.shape[1]  # length of x-axis
+# Number of pixels on y_axis
+y_axis_len = image.shape[0]  # length of y-axis
+# Number of pixels in array
+sum = x_axis_len * y_axis_len
+
+
+print("Number of non-water-pixels = ")
+number_of_zeroes = np.count_nonzero(image == 0)
+print(number_of_zeroes)
+print((number_of_zeroes/sum)*100, "%")
+
+print("Number of water-pixels = ")
+number_of_water = np.count_nonzero(image == 1)
+print((number_of_water/sum)*100, "%")
+print(number_of_water)
+
+
 
 print("threshold of separation = " + str(thresh))
